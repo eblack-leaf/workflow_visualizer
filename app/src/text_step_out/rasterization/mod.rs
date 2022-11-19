a9b95f28da4f07558ad9b3db0b0c0b4ded79cef4
omi-voshuli <rgb@colors.org>
2022-11-19 17:25:56 -0500

    text step out

--- app/src/text_step_out/rasterization/mod.rs
+++ app/src/text_step_out/rasterization/mod.rs
@@ -2,3 +2,9 @@ mod rasterizations;
 pub mod placement;
 mod rasterize;
 mod references;
+// rasterize writes - hook into writes and raster them
+// integrate appended rasterizations + shrink removes
+// so rewrite from cpu_buffer + appended rasterizations
+// once resolved send rast. placements back to entity
+// remove refs to removes
+//
