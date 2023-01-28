21f37e6d074cfaa1b88654ac0ed755fed3cda520
eblack-leaf <eblack.leaf@proton.me>
2023-01-28 13:35:53 -0500

    flat engen rewrite

--- flat_app/src/main.rs
+++ flat_app/src/main.rs
@@ -8,21 +8,24 @@ fn main() {
     let engen_descriptor = flat_engen::EngenDescriptor::new()
         .with_canvas_options(flat_engen::CanvasOptions::default())
         .with_theme(flat_engen::Theme::default())
-        .with_native_dimensions(flat_engen::Area::new(700 as f32, 500 as f32))
+        .with_native_dimensions(flat_engen::Area::new(400 as f32, 600 as f32))
         .with_min_canvas_dimensions(flat_engen::Area::new(400 as f32, 300 as f32));
     let mut engen = flat_engen::Engen::new(engen_descriptor);
     #[cfg(not(target_arch = "wasm32"))]
     {
         let args: Vec<String> = std::env::args().collect();
         if let Some(param) = args.get(1) {
-            if param == "web-serve" {
+            if param.starts_with("web") {
                 let compile_descriptor =
                     flat_engen::CompileDescriptor::new("flat_app", "--debug", "flat_app_web_build");
-                engen
-                    .compile_wasm_to(compile_descriptor).expect("could not compile")
-                    .serve_at(([0, 0, 0, 0], 3030));
-                return;
+                let server = engen.compile_wasm_to(compile_descriptor).expect("could not compile");
+                if param == "web-compile" { return; }
+                if param == "web-serve" {
+                    server.serve_at(([0, 0, 0, 0], 3030));
+                    return;
+                }
             }
+
         }
     }
     // engen.add_render_attachment::<flat_engen::TextRenderer>();
