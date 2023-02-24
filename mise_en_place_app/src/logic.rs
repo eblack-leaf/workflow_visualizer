9853c1f0d8a4235a4112e9dcf3507bcdbc4ecb23
eblack-leaf <eblack.leaf@proton.me>
2023-02-24 13:24:30 -0500

    message receiver

--- mise_en_place_app/src/logic.rs
+++ mise_en_place_app/src/logic.rs
@@ -1,13 +1,13 @@
 use serde::{Deserialize, Serialize};
 
-use mise_en_place::bevy_ecs;
-use mise_en_place::bevy_ecs::change_detection::ResMut;
-use mise_en_place::bevy_ecs::entity::Entity;
-use mise_en_place::bevy_ecs::prelude::{Commands, Query, Res, Resource};
+use mise_en_place::{bevy_ecs, BundledIconKeys, Clickable, ClickListener, Color, FrontEndStages, IconBundle, IconKey, IconMesh, IconMeshAddRequest, IconSize, Job, Launch, Location, TextBoundGuide, TextBundle, TextScaleAlignment};
 use mise_en_place::{
     Area, ClickState, Exit, Icon, Idle, MessageReceiver, MessageRepr, MessageType, MouseAdapter,
     Position, Text, TouchAdapter, UIView, VirtualKeyboardAdapter, VirtualKeyboardType,
 };
+use mise_en_place::bevy_ecs::change_detection::ResMut;
+use mise_en_place::bevy_ecs::entity::Entity;
+use mise_en_place::bevy_ecs::prelude::{Commands, Query, Res, Resource};
 
 #[derive(Resource)]
 pub struct Counter {
@@ -46,7 +46,7 @@ pub fn update_text(
     let mut click_info = String::new();
     for (entity, icon, click_state, position, area) in click_icon.iter() {
         if click_state.clicked() {
-            click_info += &*format!("entity: {:?}, clicked: {:?}", entity, click_state.clicked(),);
+            click_info += &*format!("entity: {:?}, clicked: {:?}", entity, click_state.clicked(), );
             let current = counter.count;
             counter.state.replace(current);
             virtual_keyboard.open(VirtualKeyboardType::Keyboard);
@@ -59,7 +59,7 @@ pub fn update_text(
             if let Some(state) = counter.state {
                 if counter.count >= state + 100 {
                     click_info +=
-                        &*format!("entity: {:?}, clicked: {:?}", entity, click_state.clicked(),);
+                        &*format!("entity: {:?}, clicked: {:?}", entity, click_state.clicked(), );
                     counter.state.take();
                 }
             }
@@ -93,5 +93,55 @@ pub fn update_text(
                     format!("message-ty: {:?}", messages).as_str();
             }
         }
+    }
+}
+
+pub struct Launcher;
+
+impl Launch for Launcher {
+    fn prepare(job: &mut Job) {
+        job.container.insert_resource(Counter {
+            count: 0,
+            state: None,
+        });
+        job.main
+            .add_system_to_stage(FrontEndStages::Process, update_text);
+        job.container
+            .spawn(TextBundle::new(
+                Text::new(vec![("mouse location: ", (Color::OFF_WHITE, 0))]),
+                Location::from(((35u32, 10u32), 0u32)),
+                TextScaleAlignment::Medium,
+            ))
+            .insert(TextBoundGuide::new(18, 6));
+        job.container
+            .spawn(TextBundle::new(
+                Text::new(vec![("click info: ", (Color::OFF_WHITE, 0))]),
+                Location::from(((35u32, 160u32), 0u32)),
+                TextScaleAlignment::Medium,
+            ))
+            .insert(TextBoundGuide::new(44, 3));
+        job.container.spawn(IconMeshAddRequest::new(
+            IconKey("bundled box"),
+            IconMesh::bundled(BundledIconKeys::Box),
+            10,
+        ));
+        let id = job
+            .container
+            .spawn(IconBundle::new(
+                Icon {},
+                IconSize::Large,
+                IconKey("bundled box"),
+                Location::from(((10u32, 17u32), 0u32)),
+                Color::OFF_WHITE,
+            ))
+            .insert(Clickable::new(ClickListener::on_press(), false))
+            .id();
+        job.container
+            .spawn(TextBundle::new(
+                Text::new(vec![("", (Color::OFF_WHITE, 0))]),
+                Location::from(((35u32, 260u32), 0u32)),
+                TextScaleAlignment::Medium,
+            ))
+            .insert(TextBoundGuide::new(38, 20));
     }
 }
