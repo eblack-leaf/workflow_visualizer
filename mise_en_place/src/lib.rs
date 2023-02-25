0d6bb78899e8c127ed4e26dea458ceb4ef6d1e8c
eblack-leaf <eblack.leaf@proton.me>
2023-02-25 18:07:04 -0500

    conditional authentication

--- mise_en_place/src/lib.rs
+++ mise_en_place/src/lib.rs
@@ -1,18 +1,18 @@
 pub use bevy_ecs;
 
-pub use engen::BackEndStartupStages;
+pub use engen::{Attach, Engen, Launch};
+pub use engen::{ExecutionState, Exit, Idle, Job, Task};
 pub use engen::BackendStages;
+pub use engen::BackEndStartupStages;
 pub use engen::EngenOptions;
 pub use engen::FrontEndStages;
 pub use engen::FrontEndStartupStages;
-pub use engen::{Attach, Engen, Launch};
-pub use engen::{ExecutionState, Exit, Idle, Job, Task};
 pub use visibility::VisibleBounds;
 pub use wasm_compiler::WasmCompiler;
 #[cfg(not(target_arch = "wasm32"))]
 pub use wasm_server::WasmServer;
 
-pub use crate::clickable::{ClickListener, ClickState, Clickable};
+pub use crate::clickable::{Clickable, ClickListener, ClickState};
 pub use crate::color::Color;
 pub use crate::coord::{
     Area, AreaAdjust, Depth, DepthAdjust, DeviceView, GpuArea, GpuPosition, Location, Numerical,
@@ -20,8 +20,8 @@ pub use crate::coord::{
 };
 pub use crate::gfx::{Viewport, ViewportPlugin};
 pub use crate::icon::{
-    read_mesh, write_mesh, BundledIconKeys, ColorHooks, ColorInvert, Icon, IconBundle, IconKey,
-    IconMesh, IconMeshAddRequest, IconPlugin, IconSize, IconVertex,
+    BundledIconKeys, ColorHooks, ColorInvert, Icon, IconBundle, IconKey, IconMesh, IconMeshAddRequest,
+    IconPlugin, IconSize, IconVertex, read_mesh, write_mesh,
 };
 pub use crate::text::{
     PartitionMetadata, Text, TextBoundGuide, TextBundle, TextPartition, TextPlugin,
@@ -48,7 +48,6 @@ mod text;
 mod theme;
 mod uniform;
 mod visibility;
-mod wasm;
 mod wasm_compiler;
 #[cfg(not(target_arch = "wasm32"))]
 #[allow(unused)]
