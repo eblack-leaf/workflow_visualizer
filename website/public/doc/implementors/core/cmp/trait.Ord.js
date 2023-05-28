2fb18fc1d232fff66af06867424b058c305b8959
eblack-leaf <eblack.leaf@proton.me>
2023-05-28 15:54:40 -0400

    web workflow implemented + blanket native runner

---
+++ website/public/doc/implementors/core/cmp/trait.Ord.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"workflow_visualizer":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.68.2/core/cmp/trait.Ord.html\" title=\"trait core::cmp::Ord\">Ord</a> for <a class=\"struct\" href=\"workflow_visualizer/struct.Key.html\" title=\"struct workflow_visualizer::Key\">Key</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.68.2/core/cmp/trait.Ord.html\" title=\"trait core::cmp::Ord\">Ord</a> for <a class=\"struct\" href=\"workflow_visualizer/struct.TextGridLocation.html\" title=\"struct workflow_visualizer::TextGridLocation\">TextGridLocation</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
--- website/src/main.rs
+++ website/src/main.rs
@@ -1,13 +1,34 @@
 use dioxus::prelude::*;
 use dioxus_free_icons::Icon;
+use dioxus_router::{Link, Route, Router};
 
 fn main() {
     #[cfg(target_family = "wasm")]
-    dioxus_web::launch(Root);
+    dioxus_web::launch(root);
 }
-fn Root(cx: Scope) -> Element {
+fn root(cx: Scope) -> Element {
     let rsx = rsx!(
+        Router {
+            Route { to: "/", landing(cx) }
+            Route { to: "/architecture", architecture(cx)}
+        }
+    );
+    cx.render(rsx)
+}
+fn landing(cx: Scope) -> Element {
+    let rsx = rsx!(
+        div {
+            class: "font-mono text-neutral-300 bg-neutral-800",
+            div {}
+            Link { to: "doc/workflow_visualizer/index.html", external: true, "API Reference"}
+        }
 
+    );
+    cx.render(rsx)
+}
+fn architecture(cx: Scope) -> Element {
+    let rsx = rsx!(
+        p { "hello world... architecture"}
     );
     cx.render(rsx)
 }
