c4664415342909e32f7f58e676599f7b3614ba18
eblack-leaf <eblack.leaf@proton.me>
2023-08-31 21:35:16 -0400

    fix

--- workflow_visualizer/src/workflow/run.rs
+++ workflow_visualizer/src/workflow/run.rs
@@ -17,12 +17,11 @@ pub(crate) fn internal_loop<T: Workflow + 'static>(
     control_flow: &mut ControlFlow,
     desktop_dimensions: Option<Area<DeviceContext>>,
 ) {
-    control_flow.set_poll();
-    // if visualizer.can_idle() {
-    //     control_flow.set_wait();
-    // } else {
-    //     control_flow.set_poll();
-    // }
+    if visualizer.can_idle() {
+        control_flow.set_wait();
+    } else {
+        control_flow.set_poll();
+    }
     match event {
         Event::NewEvents(cause) => match cause {
             StartCause::Init => {
