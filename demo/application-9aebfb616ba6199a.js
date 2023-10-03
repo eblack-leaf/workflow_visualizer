bfd16eb1ee893828a8a2df36c259528dd0e116c8
eblack-leaf <eblack.leaf@proton.me>
2023-10-03 07:04:17 +0000

    website_push

--- demo/application-9aebfb616ba6199a.js
+++ demo/application-9aebfb616ba6199a.js
@@ -1830,7 +1830,7 @@ async function __wbg_init(input) {
     if (wasm !== undefined) return wasm;
 
     if (typeof input === 'undefined') {
-        input = new URL('application-9f5b4f0a591ec4b9_bg.wasm', import.meta.url);
+        input = new URL('application-9aebfb616ba6199a_bg.wasm', import.meta.url);
     }
     const imports = __wbg_get_imports();
 
